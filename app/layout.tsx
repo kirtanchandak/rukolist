a9cba10fc77680490158cea1a62304a33e108362
kirtanchandak <themarketingkirtan@gmail.com>
2024-09-07 22:43:52 +0530

    added design to the pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import "./globals.css";
+import AuthButton from "@/components/header-auth";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -25,47 +23,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
-        </ThemeProvider>
+      <body className="flex flex-col min-h-screen bg-background text-foreground">
+        <div className="flex items-center justify-between md:px-20 px-4 py-6 bg-primary">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              className="rounded-full"
+              width={40}
+              height={40}
+              src="https://play-lh.googleusercontent.com/O8mvDQlw4AwmGfUrh4lviZD_PwwhRHz2etA25F77SbXrm3qEHOt2826aNkKar4D0yw"
+              alt="logo"
+            />
+            <p className="text-2xl font-bold text-secondary">rukolist</p>
+          </Link>
+          <AuthButton />
+        </div>
+        <main className="flex-grow">{children}</main>
+        <footer className="bg-primary text-secondary w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
+          <p>
+            Powered by{" "}
+            <a
+              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              target="_blank"
+              className="font-bold hover:underline"
+              rel="noreferrer"
+            >
+              rukolist
+            </a>
+          </p>
+        </footer>
       </body>
     </html>
   );
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,44 +1,32 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
-export default function Header() {
+export default function Hero() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
+    <>
+      <div className="flex items-center justify-center py-24 bg-primary">
+        <div className="h-screen">
+          <h1 className="text-4xl font-extrabold text-center text-secondary">
+            Everything you need to launch. <br /> In one, simple link.
+          </h1>
+          <form className="flex flex-col items-center space-y-4 mt-4">
+            <div className="flex items-center border border-gray-300 rounded-md overflow-hidden">
+              <span className="px-4 py-2 text-secondary">rukolist.com/</span>
+              <input
+                type="text"
+                className="flex-1 focus:outline-none bg-primary text-secondary"
+                placeholder="yourproduct"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="py-2 px-4 bg-secondary text-primary rounded-full flex items-center gap-1"
+            >
+              Launch now <IoIosArrowRoundForward className="text-2xl" />
+            </button>
+          </form>
+        </div>
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-    </div>
+    </>
   );
 }
