eed33c781995904599686412d48858ebd8e18d48
kirtanchandak <themarketingkirtan@gmail.com>
2024-09-11 22:55:59 +0530

    otpless init

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import { signInAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
@@ -6,52 +9,24 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { OAuthButtons } from "../oAuth-signin";
 
+declare global {
+  interface Window {
+    otpless: (user: any) => void;
+  }
+}
+
 export default function Login({ searchParams }: { searchParams: Message }) {
+  useEffect(() => {
+    window.otpless = (otplessUser) => {
+      console.log(otplessUser);
+    };
+  }, []);
+
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen">
-        <form className="w-full max-w-md">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-medium">Sign in</h1>
-            <p className="text-sm text-foreground">
-              Don't have an account?{" "}
-              <Link
-                className="text-foreground font-medium underline"
-                href="/sign-up"
-              >
-                Sign up
-              </Link>
-            </p>
-            <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8 w-full">
-              <Label htmlFor="email">Email</Label>
-              <Input name="email" placeholder="you@example.com" required />
-              <div className="flex justify-between items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  className="text-xs text-foreground underline"
-                  href="/forgot-password"
-                >
-                  Forgot Password?
-                </Link>
-              </div>
-              <Input
-                type="password"
-                name="password"
-                placeholder="Your password"
-                required
-              />
-              <SubmitButton
-                pendingText="Signing In..."
-                formAction={signInAction}
-                className="text-white"
-              >
-                Sign in
-              </SubmitButton>
-              <OAuthButtons />
-              <FormMessage message={searchParams} />
-            </div>
-          </div>
-        </form>
+      {/* OTP-less Login */}
+      <div className="flex items-center justify-center mt-14">
+        <div id="otpless-login-page"></div>
       </div>
     </>
   );
