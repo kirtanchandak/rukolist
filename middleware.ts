67de3d6873b9565d3997a9ed593454bc494d2785
kirtanchandak <themarketingkirtan@gmail.com>
2024-09-13 16:13:46 +0530

    middlware fix

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,19 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request);
+
+  //@ts-ignore
+  if (!response?.user) {
+    const signInUrl = new URL("/sign-in", request.url);
+    signInUrl.searchParams.set("redirect", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/launchproduct",
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/launchproduct", "/admin"],
 };
