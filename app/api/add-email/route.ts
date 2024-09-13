bf324e090501ab8361e8d3be91f2421e3cda1dec
kirtanchandak <themarketingkirtan@gmail.com>
2024-09-13 16:23:19 +0530

    react-host-toast fix

--- app/api/add-email/route.ts
+++ app/api/add-email/route.ts
@@ -1,46 +1,42 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@/utils/supabase/client";
+import { NextRequest, NextResponse } from "next/server";
 
 // Supabase client initialization
 const supabase = createClient();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
+export async function POST(req: NextRequest) {
+  try {
     const { email, productname }: { email: string; productname: string } =
-      req.body;
+      await req.json();
 
     // Validate email
     if (!email || !email.includes("@")) {
-      return res.status(400).json({ error: "A valid email is required" });
+      return NextResponse.json(
+        { error: "A valid email is required" },
+        { status: 400 }
+      );
     }
 
-    try {
-      // Update the correct row by productname and append the email to the emails array
-      const { data, error } = await supabase
-        .from("products") // Your products or waitlist table
-        .update({
-          emails: supabase.rpc("array_append", {
-            column: "emails",
-            value: email,
-          }),
-        }) // Append email to emails array
-        .eq("productname", productname); // Find row by productname
+    // Update the correct row by productname and append the email to the emails array
+    const { data, error } = await supabase
+      .from("products") // Your products or waitlist table
+      .update({
+        emails: supabase.rpc("array_append", {
+          column: "emails",
+          value: email,
+        }),
+      }) // Append email to emails array
+      .eq("productname", productname); // Find row by productname
 
-      if (error) {
-        throw error;
-      }
-
-      return res
-        .status(200)
-        .json({ message: "Email added to the waitlist successfully", data });
-    } catch (error: any) {
-      return res.status(500).json({ error: error.message });
+    if (error) {
+      throw error;
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).json({ error: `Method ${req.method} not allowed` });
+
+    return NextResponse.json(
+      { message: "Email added to the waitlist successfully", data },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
